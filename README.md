b6a05aa82c5a529ff44b7b378f21fe93e0be1c52
ninomihovilic <nino.mihovilic@gmail.com>
2022-08-10 15:07:56 +0200

    Add Sensei usage tracking auto-opt in

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wc-usage-tracking-auto-opt-in
-Automatically opts site into WooCommerce Usage Tracking
+Automatically opts site into WooCommerce Usage Tracking and Sensei Usage Tracking
 
 This plugin would work well as an mu-plugin that is auto-loaded into new sites upon site creation.
 
--- wc-usage-tracking-auto-opt-in.php
+++ wc-usage-tracking-auto-opt-in.php
@@ -1,14 +1,37 @@
 <?php
 /*
-Plugin Name: WooCommerce Usage Tracking Auto-opt-in
+Plugin Name: Usage Tracking Auto-opt-in
 Plugin URI: https://github.com/a8cteam51/wc-usage-tracking-auto-opt-in
-Description: Automatically opts site into WooCommerce Usage Tracking
+Description: Automatically opts site into WooCommerce Usage Tracking and Sensei Usage Tracking
 Version: 1.0.0
 Author: WP Special Projects
 */
+
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_filter( 'option_woocommerce_allow_tracking', function(){ return 'yes'; }, 'woocommerce_allow_tracking' );
+/**
+ * WooCommerce Usage Tracking Auto-opt-in
+ */
+add_filter(
+	'option_woocommerce_allow_tracking',
+	function() {
+		return 'yes';
+	},
+	'woocommerce_allow_tracking'
+);
+
+/**
+ * Sensei Usage Tracking Auto-opt-in
+ */
+add_filter(
+	'option_sensei-settings',
+	function( $values ) {
+		if ( is_array( $values ) && isset( $values['sensei_usage_tracking_enabled'] ) ) {
+			$values['sensei_usage_tracking_enabled'] = true;
+		}
+		return $values;
+	}
+);
