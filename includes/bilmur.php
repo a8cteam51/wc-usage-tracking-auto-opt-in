8a2a97e383d97b564f0f6b3812d3a3ec5d85364e
Antonius Hegyes <tony.hegyes@automattic.com>
2023-07-13 10:06:57 +0200

    Constant-based filling of bilmur properties

--- includes/bilmur.php
+++ includes/bilmur.php
@@ -13,19 +13,26 @@ if ( ! defined( 'WPCOMSP_BILMUR_TRACKING' ) || ! WPCOMSP_BILMUR_TRACKING ) {
 add_action(
 	'wp_enqueue_scripts',
 	static function() {
-		wp_enqueue_script( 'wpcomsp-bilmur', 'https://s0.wp.com/wp-content/js/bilmur.min.js?m=202215', array(), '1.0.0', true );
+		wp_enqueue_script( 'bilmur', 'https://s0.wp.com/wp-content/js/bilmur.min.js?m=202215', array(), '1.0.0', true );
 	}
 );
 
 add_filter(
 	'script_loader_tag',
 	function( $tag, $handle ) {
-		if ( 'wpcomsp-bilmur' !== $handle ) {
-			return $tag;
+		if ( 'bilmur' === $handle ) {
+			if ( defined( 'WPCOMSP_BILMUR_PROVIDER' ) ) {
+				$tag = str_replace( ' src', ' data-provider="' . WPCOMSP_BILMUR_PROVIDER . '" src', $tag );
+			}
+			if ( defined( 'WPCOMSP_BILMUR_SERVICE' ) ) {
+				$tag = str_replace( ' src', ' data-service="' . WPCOMSP_BILMUR_SERVICE . '" src', $tag );
+			}
+			if ( defined( 'WPCOMSP_BILMUR_CUSTOM_PROPERTIES' ) ) {
+				$tag = str_replace( ' src', ' data-customproperties="' . wp_json_encode( WPCOMSP_BILMUR_CUSTOM_PROPERTIES ) . '" src', $tag );
+			}
 		}
 
-		// TODO: Values for provider and service TBD. Here's the placeholder for now.
-		return str_replace( ' src', ' data-provider="" data-service="" src', $tag );
+		return $tag;
 	},
 	10,
 	2
